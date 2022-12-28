f93986d3bd64de1cd58526ce71df9d6ae90fd652
okoth kongo <okothkongo@gmail.com>
2022-12-28 07:44:33 +0300

    all the servers

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// server
+// Data
 
-const express = require('express')
+
 const {faker} = require("@faker-js/faker/locale/en")
-const app = express()
-const port = 3000
 
+
+const  currentDate = () => new Date
+const  six_hours_ago = () => new Date(currentDate().getTime()- (60 * 60 * 1000 * 6))
 function company_constructor(){
   return {
     country: "USA",
@@ -13,17 +14,33 @@ function company_constructor(){
     name: faker.company.name(),
     symbol: faker.company.name().substring(0, 3),
     category: faker.commerce.department(),
-    stock_price: Number(faker.finance.amount())
+    stock_price: Number(faker.finance.amount()),
+    timestamp: faker.date.between(six_hours_ago(), new Date )
   };
 
 }
 
-const range = Array.from({length: 100}, (_, index) => index + 1)
-const all_companies = range.map(_ => company_constructor() );
+const range = Array.from({length: 1000}, (_, index) => index + 1)
+const allCompanies = range.map(_ => company_constructor() );
+
+const newCompanies = (timestamp) => allCompanies.filter(company => company.timestamp >= timestamp  );
 
 
-app.get('/api/companies/new_companies', (req, res) => {
-  res.send({data: all_companies})
+// Rest API
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/api/companies/:timestamp', (req, res) => {
+   const timestamp = req.params.timestamp 
+   date = new Date(timestamp)
+
+  if (isNaN(Number(timestamp)) &&  date != "Invalid Date")  {    
+    res.send({data:   newCompanies(date)})
+  } else {
+  
+    res.send({data: {error: "invalid date time format"}})
+  }
 
 })
 
@@ -38,6 +55,11 @@ const { Server } = require('ws');
  
 const sockserver = new Server({ port: 5000 });
 sockserver.on('connection', (ws) => {
+   sockserver.clients.forEach((client) => {
+      const data = JSON.stringify({data: allCompanies});
+       client.send(data);
+     
+  })
    console.log('New client connected!'); 
   
 });
@@ -45,13 +67,14 @@ sockserver.on('connection', (ws) => {
 
 setInterval(() => {
   sockserver.clients.forEach((client) => {
-      const data = JSON.stringify({data: all_companies});
-      let data1 = client.send(data);
-      data1
+      const data = JSON.stringify({data: allCompanies});
+     client.send(data);
+      
   });
-}, 1200000);
+}, 3000 * 1000); // you cant around with value to increase or decrease push interval
 
-// client
+
+// websocket cleint
 
 const {Socket, Channel} = require("phoenix-channels")
 let client_socket = new Socket("ws://localhost:4000/socket/new_companies")
